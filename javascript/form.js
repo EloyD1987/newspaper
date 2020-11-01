1c1f02ee9408913aff8a5a52da69728b653099c5
Eloy Dordolo <eloydordolo87@gmail.com>
2020-11-01 19:58:11 -0300

    error messages, visibility..testing js elements

---
+++ javascript/form.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+
+var fName = document.querySelector("#name");
+
+var pText = fName.nextElementSibling;
+
+console.log(pText);
+}
+/*
+name.onblur = function validation(){
+    Name();
+
+
+function fName(){
+    var sPname = pname.value;
+    if (sPname.indexOf(" ") == -1 || sPname.replace(/ /g, "").length < 7) {
+        //alert("Nombre \"" + sPname + "\" incorrecto. Debe contener al menos 6 carácteres y un espacio en blanco.");
+       var error1 = document.getElementsByTagName("p")
+       error1.style.hidden = 
+    }
+    console.log("Personal Name: " + sPname);
+    return sPname;
+}
+
+
+name.onfocus = function() {hide()};
+
+function show(error){
+    document.getElementById("emessage").innerHTML = error;
+    document.getElementById("error").style.display = "block";
+}
+
+function hide(){
+    document.getElementById("error").style.display = "none";
+}*/

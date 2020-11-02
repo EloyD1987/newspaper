6238c5c6bb5c599cc666229ea09245b0a68236de
Eloy Dordolo <eloydordolo87@gmail.com>
2020-11-02 09:24:33 -0300

    name and email inputs ok

--- javascript/form.js
+++ javascript/form.js
@@ -1,38 +1,46 @@
-window.onload = function(){
+window.onload = function(){}
 
-var fName = document.querySelector("#name");
-
-var pText = fName.nextElementSibling;
-
-pText.style.visivility = visible ;
-console.log(pText);
-}
-
-pText.style.visivility = visible ;
-/*
-name.onblur = function validation(){
-    Name();
-
-
-function fName(){
-    var sPname = pname.value;
-    if (sPname.indexOf(" ") == -1 || sPname.replace(/ /g, "").length < 7) {
-        //alert("Nombre \"" + sPname + "\" incorrecto. Debe contener al menos 6 carácteres y un espacio en blanco.");
-       var error1 = document.getElementsByTagName("p")
-       error1.style.hidden = 
+//Input Full Name    
+var vName = document.getElementById("name");
+//Blur event
+vName.addEventListener("blur", fName);
+//Focus Event
+vName.addEventListener("focus", hidden);
+//Function validation of the value  
+function fName(e){
+    e.preventDefault();
+    var nInput = vName.value;
+    if ((nInput.indexOf(" ") == -1) || (nInput.length < 7)) { 
+       var errormessage = document.getElementById("name").nextElementSibling;
+       errormessage.style.visibility =  "visible" ;
     }
-    console.log("Personal Name: " + sPname);
-    return sPname;
+    console.log("Full Name: " + nInput);
+    return nInput;
+}
+function hidden(){
+    var errormessage = document.getElementById("name").nextElementSibling;
+    errormessage.style.visibility =  "hidden" ;
 }
 
-
-name.onfocus = function() {hide()};
-
-function show(error){
-    document.getElementById("emessage").innerHTML = error;
-    document.getElementById("error").style.display = "block";
+//Input Email    
+var vEmail = document.getElementById("email");
+vEmail.addEventListener("blur", fEmail);
+vEmail.addEventListener("focus", hiddenEmail);
+//Function validation of the value  
+function fEmail(e){
+    e.preventDefault();
+    var eInput = vEmail.value;
+    var val =/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!val.test(eInput)){
+       var errormessage = document.getElementById("email").nextElementSibling;
+       errormessage.style.visibility =  "visible" ;
+    }
+    console.log("Email: " + eInput);
+    return eInput;
 }
 
-function hide(){
-    document.getElementById("error").style.display = "none";
-}*/
+function hiddenEmail(){
+    var errormessage = document.getElementById("email").nextElementSibling;
+    errormessage.style.visibility =  "hidden" ;
+}
+

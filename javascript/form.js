8236983d7c92f6dec8af319b74d8cf0fd2dc501b
Eloy Dordolo <eloydordolo87@gmail.com>
2020-11-02 10:36:53 -0300

    password and cpassword ok

--- javascript/form.js
+++ javascript/form.js
@@ -2,13 +2,12 @@ window.onload = function(){}
 
 //Input Full Name    
 var vName = document.getElementById("name");
-//Blur event
+//Blur event = show error
 vName.addEventListener("blur", fName);
-//Focus Event
+//Focus Event = hidden error
 vName.addEventListener("focus", hidden);
 //Function validation of the value  
-function fName(e){
-    e.preventDefault();
+function fName(){
     var nInput = vName.value;
     if ((nInput.indexOf(" ") == -1) || (nInput.length < 7)) { 
        var errormessage = document.getElementById("name").nextElementSibling;
@@ -24,11 +23,12 @@ function hidden(){
 
 //Input Email    
 var vEmail = document.getElementById("email");
+//Blur event = show error
 vEmail.addEventListener("blur", fEmail);
+//Focus Event = hidden error
 vEmail.addEventListener("focus", hiddenEmail);
 //Function validation of the value  
-function fEmail(e){
-    e.preventDefault();
+function fEmail(){
     var eInput = vEmail.value;
     var val =/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if (!val.test(eInput)){
@@ -38,9 +38,53 @@ function fEmail(e){
     console.log("Email: " + eInput);
     return eInput;
 }
-
 function hiddenEmail(){
     var errormessage = document.getElementById("email").nextElementSibling;
     errormessage.style.visibility =  "hidden" ;
 }
+//Input Password    
+var vPassword = document.getElementById("pass");
+//Blur event = show error
+vPassword.addEventListener("blur",fPassword);
+//Focus Event = hidden error
+vPassword.addEventListener("focus", hiddenPassword);
+//Function validation of the value  
 
+function fPassword(){
+    var pInput = vPassword.value;
+    var val = /[^A-Za-z0-9]/;
+    if ( pInput.length<8 || val.test(pInput) ) { 
+       var errormessage = document.getElementById("pass").nextElementSibling;
+       errormessage.style.visibility =  "visible" ;
+    }
+    console.log("Password: " + pInput);
+    return pInput;
+}
+function hiddenPassword(){
+    var errormessage = document.getElementById("pass").nextElementSibling;
+    errormessage.style.visibility =  "hidden" ;
+}
+
+
+//Repeat Password    
+var vcPassword = document.getElementById("cpass");
+//Blur event = show error
+vcPassword.addEventListener("blur",fcPassword);
+//Focus Event = hidden error
+vcPassword.addEventListener("focus", hiddencPassword);
+//Function validation of the value  
+
+function fcPassword(){
+    var cpInput = vcPassword.value;
+    var val = /[^A-Za-z0-9]/;
+    if ( cpInput.length<8 || val.test(cpInput) ) { 
+       var errormessage = document.getElementById("cpass").nextElementSibling;
+       errormessage.style.visibility =  "visible" ;
+    }
+    console.log("Confirm Password: " + cpInput);
+    return cpInput;
+}
+function hiddencPassword(){
+    var errormessage = document.getElementById("cpass").nextElementSibling;
+    errormessage.style.visibility =  "hidden" ;
+}
